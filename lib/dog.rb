c0ac417a746659060521b7ea9807982840877caf
Zachary Durr <zdurr22@gmail.com>
2020-10-21 23:11:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+def name=(dogs_name)
+  @this_dogs_name = dogs_name
+end
+
+def name
+  puts "#{this_dogs_name}"
+end
+
+def bark
+  puts "Woof!"
+end
+
+def sit
+  puts "The Dog is sitting"
+end
+
+end
