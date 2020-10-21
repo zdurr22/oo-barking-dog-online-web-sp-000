c40fcbcfef15b85267dd5a9f5799def1c41e4474
Zachary Durr <zdurr22@gmail.com>
2020-10-21 23:12:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,17 @@
 class Dog
-def name=(dogs_name)
-  @this_dogs_name = dogs_name
-end
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
+  end
 
-def name
-  puts "#{this_dogs_name}"
-end
+  def name
+    puts "#{@this_dogs_name}"
+  end
 
-def bark
-  puts "Woof!"
-end
+  def bark
+    puts "Woof!"
+  end
 
-def sit
-  puts "The Dog is sitting"
-end
-
+  def sit
+    puts "The Dog is sitting"
+  end
 end
